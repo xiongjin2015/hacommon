ed1ad97e3573546688a53ce97e9808c32fd4a939
xiongjin <xiongjin02@baidu.com>
2015-08-04 15:24:59 +0800

    add  some config info

--- src/com/haha/common/config/HaConfig.java
+++ src/com/haha/common/config/HaConfig.java
@@ -1,3 +1,4 @@
+
 package com.haha.common.config;
 
 import java.io.IOException;
@@ -8,85 +9,110 @@ import android.content.Context;
 import android.content.res.AssetManager;
 
 public final class HaConfig {
-    
+
     private final static String TAG = "HaConfig";
-    
-    //config ini file name
+
+    // config ini file name
     private final String CONFIG_FILE_NAME = "config.ini";
-    
-    //properties object of the configure
+
+    // properties object of the configure
     private Properties properties = new Properties();
-    
-  //singleton instance of configure object
+
+    // singleton instance of configure object
     private static HaConfig instance;
-    
-    public static HaConfig getInstance(){
-        if(instance == null)
+
+    public static HaConfig getInstance() {
+        if (instance == null)
             instance = new HaConfig();
         return instance;
     }
-    
+
     /**
-     * initialize the {@properties} object, must be invoked first before access anything
-     * of this class
+     * initialize the {@properties} object, must be invoked first before access anything of this
+     * class
+     * 
      * @param path
      */
-    public void init(Context context){
-        /*initialize the inner properties with inner configure file*/
+    public void init(Context context) {
+        /* initialize the inner properties with inner configure file */
         this.initWithInner(context);
-        
-        /*initialize the outer properties with outer configure file*/
-        //this.initWithOuter();
+
+        /* initialize the outer properties with outer configure file */
+        // this.initWithOuter();
     }
-    
-    private void initWithInner(Context context){
+
+    private void initWithInner(Context context) {
         InputStream is = null;
-        try{
+        try {
             AssetManager am = context.getAssets();
             is = am.open(CONFIG_FILE_NAME);
             properties.load(is);
-        }catch(Exception e){
-            if(is != null){
+        } catch (Exception e) {
+            if (is != null) {
                 try {
                     is.close();
-                } catch (IOException e1) {}
+                } catch (IOException e1) {
+                }
             }
         }
     }
-    
-    public long getLong(ConfigID id){
-        return this.getLong(id, Long.valueOf(id.getDefaultValue()).longValue());
+
+    public int getInt(ConfigID id) {
+        return this.getInt(id, Integer.valueOf(id.getDefaultValue()).intValue());
     }
-    
-    public long getLong(ConfigID id, long defaultValue){
+
+    public int getInt(ConfigID id, int defaultValue) {
         String val = properties.getProperty(id.getKey(), null);
-        if(val != null){
-            try{
-                return Long.valueOf(val).longValue();
-            }catch(Exception e){
+        if (val != null) {
+            try {
+                return Integer.valueOf(val).intValue();
+            } catch (Exception e) {
                 return defaultValue;
             }
         }
-        
+
         return defaultValue;
     }
-    
-    public enum ConfigID{
-        CACHE_RULE_UPDATE_TIME_LIMIT("cache_rule_update_time_limit", "259200000");//in million seconds
-        
+
+    public long getLong(ConfigID id) {
+        return this.getLong(id, Long.valueOf(id.getDefaultValue()).longValue());
+    }
+
+    public long getLong(ConfigID id, long defaultValue) {
+        String val = properties.getProperty(id.getKey(), null);
+        if (val != null) {
+            try {
+                return Long.valueOf(val).longValue();
+            } catch (Exception e) {
+                return defaultValue;
+            }
+        }
+
+        return defaultValue;
+    }
+
+    public enum ConfigID {
+        CACHE_RULE_UPDATE_TIME_LIMIT("cache_rule_update_time_limit", "259200000"), // in million
+                                                                                   // seconds
+
+        /* imagerloader related */
+        WIDGET_IAMGELOADER_MEMORY_CACHE_SIZE("widget_imageloader_memory_cache_size", "6"), // imageLoader
+        WIDGET_IAMGELOADER_DISK_CACHE_SIZE("widget_imageloader_disk_cache_size", "16"),
+        WIDGET_IAMGELOADER_LOAD_THREAD_SIZE("widget_afinal_load_thread_size", "5");
+
         private String key;
         private String defaultValue;
-        
-        ConfigID(String key,String defaultValue){
+
+        ConfigID(String key, String defaultValue) {
             this.key = key;
             this.defaultValue = defaultValue;
         }
-        
-        public String getKey(){
+
+        public String getKey() {
             return key;
         }
-        
-        public String getDefaultValue(){
+
+        public String getDefaultValue() {
             return defaultValue;
         }
     }
